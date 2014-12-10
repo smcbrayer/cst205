0433e697ffe4e20725655120bb351809943f33b6
Maria Genitempo <mariagenitempo@gmail.com>
2014-12-09 21:05:38 -0800

    Add Lab 14 part 2 code
    Added Lab 14 part 2 code to bottom of file

--- Lab14.py
+++ Lab14.py
@@ -42,4 +42,27 @@ def textStats():
   printNow("The total number of words in the text is : " + str(len(wordList)))
 
   
+def getHeadlines():
+  myFile = open('C:\\Users\\maria\\Documents\\Projects - School\\CST 205\\otterrealm.html', 'rt')
+  news = myFile.read()
+  myFile.close()
   
+  headlines = ""
+  
+  #the column where the headlines are located, so we don't have to search the entire page.
+  otterNewsStart = news.find('<div class="column mid-column">')
+
+  headlineLocation = otterNewsStart  #Starting place to look for headlines
+
+  if otterNewsStart <> -1: #make sure the center column exists
+     # loop through links wrapping headlines, we want text only
+     headlineLocation = news.find('rel="bookmark">\n',headlineLocation)
+     while (headlineLocation <> -1):
+       headlineStart = headlineLocation + 17 # characters in rel="bookmark>\n"
+       headlineEnd = news.find("</a>",headlineStart+1) #find closing anchor tag
+       headline = news[headlineStart:headlineEnd]
+       #this replace characters is messy
+       headline = news[headlineStart:headlineEnd].lstrip().replace("&nbsp;", " ").replace("\xE2\x80\x99","'").replace("&#8230;","...")
+       headlines = headlines + headline + "\n" #create a list of the headlines
+       headlineLocation = news.find('rel="bookmark"',headlineLocation+1) #find the next headline
+  print headlines
