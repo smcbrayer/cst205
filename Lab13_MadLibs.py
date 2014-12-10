a4f3045af1c0f97113e40d9c624c2d00d1de016d
Maria Genitempo <mariagenitempo@gmail.com>
2014-12-09 21:32:52 -0800

    Combine string replacements for each section
    combine string replacements for each section to fix bug where lines were
    duplicated with each replacement.

--- Lab13_MadLibs.py
+++ Lab13_MadLibs.py
@@ -1,14 +1,20 @@
 #Lab 13
+########
+# Team 8
+# Maria Genitempo
+# Jeremy Fransen
+# Shawn McBrayer
+# Chris Smith
+########
 
-
-s1 = "Missile and launch pluralNoun have been moved to the east coast of North Korea in the \"last few days,\" a U.S. official with direct knowledge of the information told CNN Thursday."
-s2 = "The apparent deployment comes amid further adverb statements by North Korea and heightened tensions in the region -- a situation that \"does not need to get hotter,\" a U.S. State Department spokeswoman said."
-s3 = "The move of the noun and launch equipment could mean that place, which unleashed another round of adverb rhetoric accusing the United States of pushing the region to the \"brink of war,\" may be planning a missile launch soon."
-s4 = "The pluralNoun, the official said, are consistent with those of a propNoun missile, which has a 2,500-mile range, meaning it could threaten South Korea, Japan and Southeast Asia."
-s5 = "N. Korea threatens \'adjective\' strikes North Korea's nuclear threats S. Koreans barred from N. Korean complex Does N. Korea have something to prove?"
-s6 = "The United States has been looking for a hidden North Korean east coast noun site or mobile launchers, a concern because a launch from the east coast would go over propNoun, the official said."
+s1 = "Missile and launch pluralNoun have been moved to the east coast of North Korea in the \"last few days,\" a U.S. official with direct knowledge of the information told CNN Thursday. "
+s2 = "The apparent deployment comes amid further adverb statements by North Korea and heightened tensions in the region -- a situation that \"does not need to get hotter,\" a U.S. State Department spokeswoman said. "
+s3 = "The move of the noun and launch equipment could mean that place, which unleashed another round of adverb rhetoric accusing the United States of pushing the region to the \"brink of war,\" may be planning a missile launch soon. "
+s4 = "The pluralNoun, the official said, are consistent with those of a propNoun missile, which has a 2,500-mile range, meaning it could threaten South Korea, Japan and Southeast Asia. "
+s5 = "N. Korea threatens adjective strikes North Korea's nuclear threats S. Koreans barred from N. Korean complex. Does N. Korea have something to prove? "
+s6 = "The United States has been looking for a hidden North Korean east coast noun site or mobile launchers, a concern because a launch from the east coast would go over propNoun, the official said. "
 s7 = "It is believed a noun launch would be a \"test\" launch rather than a targeted verb. That is because it appears the North Koreans have only moved the components so far. The United States is waiting to see whether North Korea issues a notice to its airmen and mariners to stay out of the region."
-s =""
+s = ""
 
 #s1 replacemnets: pluralNoun, 
 #s2 replacements: adverb,
@@ -27,29 +33,24 @@ def madLib():
   s += s2.replace("adverb", userResponse)
   
   userResponse = requestString("Enter a noun")
-  s += s3.replace("noun", userResponse)
   userResponse = requestString("Enter a place")
-  s += s3.replace("place", userResponse)
   userResponse = requestString("Enter an adverb")
-  s += s3.replace("adverb", userResponse)
+  s += s3.replace("place", userResponse).replace("noun", userResponse).replace("adverb", userResponse)
   
   userResponse = requestString("Enter a plural noun")
-  s += s4.replace("pluralNoun", userResponse)
   userResponse = requestString("Enter a proper noun")
-  s += s4.replace("propNoun", userResponse)
-  
+  s += s4.replace("pluralNoun", userResponse).replace("propNoun", userResponse)
+ 
   userResponse = requestString("Enter an adjective")
   s += s5.replace("adjective", userResponse)
   
   userResponse = requestString("Enter a noun")
-  s += s6.replace("noun", userResponse)
   userResponse = requestString("Enter a proper noun")
-  s += s6.replace("propNoun", userResponse)
+  s += s6.replace("noun", userResponse).replace("propNoun", userResponse)
   
   userResponse = requestString("Enter a noun")
-  s += s7.replace("noun", userResponse)
   userResponse = requestString("Enter a verb")
-  s += s7.replace("verb", userResponse)
+  s += s7.replace("noun", userResponse).replace("verb", userResponse)
   
   printNow(s)
 
